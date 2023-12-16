309a4571f8b8dfaf3601a77b45617bc79db64751
Onur Osmanov <66003294+Pand161@users.noreply.github.com>
2023-12-16 22:19:29 +0200

    added register page
    added register page

--- my-react-project/src/components/features/login/Login.jsx
+++ my-react-project/src/components/features/login/Login.jsx
@@ -1,5 +1,32 @@
+import { Link } from "react-router-dom"
+
 export default function Login() {
     return(
-        <h1>Login Page</h1>
+        <div id="login-form" className="login-form">
+        <form id="login">
+            <div className="login-container">
+
+            <div className="login-title">Login</div>
+
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" placeholder="peter@gmail.com" />
+
+                <label htmlFor="pass">Password:</label>
+                <input type="password" name="password" id="register-password" />
+
+                <div className="login-submit">
+                    <input className="btn-submit" type="submit" value="Sign Up" />
+                </div>
+                
+
+                <div className="login-div">
+                <p className="login-text">
+                        If you don't have profile click <Link to="/register">here</Link>
+                    </p>
+                </div>
+        
+            </div>
+        </form>
+    </div>
     )
 }
--- my-react-project/src/components/features/register/Register.jsx
+++ my-react-project/src/components/features/register/Register.jsx
@@ -1,5 +1,40 @@
+import { Link } from "react-router-dom"
+
 export default function Register() {
     return(
-        <h1>Register Page</h1>
+    <div id="register-form" className="register-form">
+        <form id="register">
+            <div className="register-container">
+
+            <div className="register-title">Register</div>
+
+                <label htmlFor="username">Username:</label>
+                <input type="username" id="username" name="username" placeholder="Peter..." />
+
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" placeholder="peter@gmail.com" />
+
+                <label htmlFor="pass">Password:</label>
+                <input type="password" name="password" id="register-password" />
+
+                <label htmlFor="con-pass">Confirm Password:</label>
+                <input type="password" name="confirm-password" id="confirm-password" />
+                
+                <div className="register-submit">
+                    <input className="btn-submit" type="submit" value="Sign Up" />
+                </div>
+                
+
+                <div className="register-div">
+                <p className="register-text">
+                        If you already have profile click <Link to="/login">here</Link>
+                    </p>
+                </div>
+                    {/* <p>
+                        <span>If you already have profile click <Link to="/login">here</Link></span>
+                    </p> */}
+            </div>
+        </form>
+    </div>
     )
 }
