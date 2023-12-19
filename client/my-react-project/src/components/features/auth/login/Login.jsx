1f7d98dca60676e4050b6c1ee295dbedeb781eb3
Onur Osmanov <66003294+Pand161@users.noreply.github.com>
2023-12-19 10:41:07 +0200

    login validation done
    login validation done

--- client/my-react-project/src/components/features/auth/login/Login.jsx
+++ client/my-react-project/src/components/features/auth/login/Login.jsx
@@ -1,7 +1,7 @@
-/* eslint-disable react/no-unescaped-entities */
+
 import { Link } from "react-router-dom"
 import style from "./Login.module.css"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 
 import AuthContext from "../../../../contexts/authContext"
 import useForm from "../../../../hooks/useForm"
@@ -13,7 +13,40 @@ const LoginFormKeys = {
 
 export default function Login() {
     const { loginSubmitHandler } = useContext(AuthContext);
-    const {values, onChange, onSubmit} = useForm(loginSubmitHandler ,{
+    const [errors, setErrors] = useState({});
+
+    function validateEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+      }
+
+      const emailValidator = () => {
+        if (!validateEmail(values.email)) {
+          setErrors(state => ({
+            ...state,
+            email: 'Invalid Email Adress',
+          }));
+        } else {
+          if (errors.email) {
+            setErrors(state => ({ ...state, email: '' }));
+          }
+        }
+      };
+
+      const passwordValidator = () => {
+        if (values.password.length < 5) {
+          setErrors(state => ({
+            ...state,
+            password: 'Your Password Shoul be Atleast 5 symbols',
+          }));
+        } else {
+          if (errors.password) {
+            setErrors(state => ({ ...state, password: '' }));
+          }
+        }
+      };
+
+      const {values, onChange, onSubmit} = useForm(loginSubmitHandler ,{
         [LoginFormKeys.Email]: '',
         [LoginFormKeys.Password]: '',
     })
@@ -26,13 +59,20 @@ export default function Login() {
             <div className={style.loginTitle}>Login</div>
 
                 <label htmlFor="email">Email:</label>
-                <input type="text" id="email" name="email" placeholder="peter@gmail.com" onChange={onChange} value={values[LoginFormKeys.Email]}/>
+                <input type="text" id="email" name="email" placeholder="peter@gmail.com" onChange={onChange} onBlur={emailValidator} value={values[LoginFormKeys.Email]}/>
+                {errors.email && (
+                    <p className={style.errorMessage}>{errors.email}</p>
+                  )}
 
                 <label htmlFor="pass">Password:</label>
-                <input type="password" name="password" id="login-password" onChange={onChange} value={values[LoginFormKeys.Password]} />
-
+                <input type="password" name="password" id="login-password" onChange={onChange} onBlur={passwordValidator} value={values[LoginFormKeys.Password]} />
+                {errors.password && (
+                    <p className={style.errorMessage}>{errors.password}</p>
+                  )}
+                  
                 <div className={style.loginSubmit}>
-                    <input className={style.btnSubmit} type="submit" value="Sign In" />
+                    <input className={style.btnSubmit} disabled={(Object.values(errors).some(x => x)
+                      || (Object.values(values).some(x => x == '')))}  type="submit" value="Sign In" />
                 </div>
                 
 
