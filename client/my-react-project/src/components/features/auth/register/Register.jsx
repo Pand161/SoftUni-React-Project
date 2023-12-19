486b053b9aed63d856dd030aeae12825a8cdf53a
Onur Osmanov <66003294+Pand161@users.noreply.github.com>
2023-12-19 10:58:59 +0200

    register validation done
    register validation done

--- client/my-react-project/src/components/features/auth/register/Register.jsx
+++ client/my-react-project/src/components/features/auth/register/Register.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import style from "./Register.module.css"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import AuthContext from "../../../../contexts/authContext"
 import useForm from "../../../../hooks/useForm"
 
@@ -8,11 +8,72 @@ const LoginFormKeys = {
     Username: 'username',
     Email: 'email',
     Password: 'password',
-    RepeatPassword: 'rePass'
+    RepeatPassword: 'rePassword'
 }
 
 export default function Register() {
     const { registerSubmitHandler } = useContext(AuthContext);
+    const [errors, setErrors] = useState({});
+
+    function validateEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+      }
+
+      const emailValidator = () => {
+        if (!validateEmail(values.email)) {
+          setErrors(state => ({
+            ...state,
+            email: 'Invalid Email Adress',
+          }));
+        } else {
+          if (errors.email) {
+            setErrors(state => ({ ...state, email: '' }));
+          }
+        }
+      };
+    
+      
+  const usernameValidator = () => {
+    if (values.username.length < 4) {
+      setErrors(state => ({
+        ...state,
+        username: 'Username Should be Atleast 4 symbols',
+      }));
+    } else {
+      if (errors.username) {
+        setErrors(state => ({ ...state, username: '' }));
+      }
+    }
+  };
+
+  const passwordValidator = () => {
+    if (values.password.length < 5) {
+      setErrors(state => ({
+        ...state,
+        password: 'Password Should be Atleast 5 symbols',
+      }));
+    } else {
+      if (errors.password) {
+        setErrors(state => ({ ...state, password: '' }));
+      }
+    }
+  };
+
+
+  const rePasswordValidator = () => {
+    if (values.rePassword != values.password) {
+      setErrors(state => ({
+        ...state,
+        rePassword: 'Passwords Should Match',
+      }));
+    } else {
+      if (errors.rePassword) {
+        setErrors(state => ({ ...state, rePassword: '' }));
+      }
+    }
+  };
+    
     const {values, onChange, onSubmit} = useForm(registerSubmitHandler, {
         [LoginFormKeys.Username]: '',
         [LoginFormKeys.Email]: '',
@@ -28,19 +89,33 @@ export default function Register() {
             <div className={style.registerTitle}>Register</div>
 
                 <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username" placeholder="Peter..." onChange={onChange} value={values[LoginFormKeys.Username]}/>
+                <input type="text" id="username" name="username" placeholder="Peter..." onChange={onChange} onBlur={usernameValidator} value={values[LoginFormKeys.Username]}/>
+                {errors.username && (
+                    <p className={style.errorMessage}>{errors.username}</p>
+                  )}
 
                 <label htmlFor="email">Email:</label>
-                <input type="text" id="email" name="email" placeholder="peter@gmail.com" onChange={onChange} value={values[LoginFormKeys.Email]}/>
+                <input type="text" id="email" name="email" placeholder="peter@gmail.com" onChange={onChange} onBlur={emailValidator} value={values[LoginFormKeys.Email]}/>
+                {errors.email && (
+                    <p className={style.errorMessage}>{errors.email}</p>
+                  )}
 
                 <label htmlFor="pass">Password:</label>
-                <input type="password" name="password" id="register-password" onChange={onChange} value={values[LoginFormKeys.Password]}/>
+                <input type="password" name="password" id="register-password" onChange={onChange} onBlur={passwordValidator} value={values[LoginFormKeys.Password]}/>
+                {errors.password && (
+                    <p className={style.errorMessage}>{errors.password}</p>
+                  )}
 
-                <label htmlFor="rePass">Confirm Password:</label>
-                <input type="password" name="rePass" id="rePass" onChange={onChange} value={values[LoginFormKeys.RepeatPassword]}/>
-                
+                <label htmlFor="rePassword">Confirm Password:</label>
+                <input type="password" name="rePassword" id="rePassword" onChange={onChange} onBlur={rePasswordValidator} value={values[LoginFormKeys.RepeatPassword]}/>
+                {errors.rePassword && (
+                    <p className={style.errorMessage}>{errors.rePassword}</p>
+                  )}
+
+
                 <div className={style.registerSubmit}>
-                    <input className={style.btnSubmit} type="submit" value="Sign Up" />
+                    <input className={style.btnSubmit} disabled={(Object.values(errors).some(x => x)
+                      || (Object.values(values).some(x => x == '')))} type="submit" value="Sign Up" />
                 </div>
                 
 
