40d94fa522ec2578c24a7eb2b2c3ef618b3b48d2
Onur Osmanov <66003294+Pand161@users.noreply.github.com>
2023-12-19 11:10:58 +0200

    create validation done
    create validation done

--- client/my-react-project/src/components/features/games/create-game/CreateGame.jsx
+++ client/my-react-project/src/components/features/games/create-game/CreateGame.jsx
@@ -3,6 +3,7 @@ import style from "./Create.module.css"
 import * as itemService from "../../../../services/itemService"
 import Path from "../../../../paths";
 import useForm from "../../../../hooks/useForm";
+import { useState } from 'react';
 
 const initialState = {
     title: '',
@@ -14,12 +15,78 @@ const initialState = {
 
 export default function CreateGame() {
     const navigate = useNavigate();
+    const [errors, setErrors] = useState({});
 
     const createSubmitHandler = (values) => {
 
         itemService.createItem(values)
             .then(() => navigate(Path.AllGames))
             .catch(err => console.log(err));
+    };
+
+    const titlelValidator = () => {
+        if (values.title.length < 1) {
+            setErrors(state => ({
+                ...state,
+                title: 'Title is required',
+            }));
+        } else {
+            if (errors.title) {
+                setErrors(state => ({ ...state, title: '' }));
+            }
+        }
+    };
+
+    const genrelValidator = () => {
+        if (values.genre.length < 1) {
+            setErrors(state => ({
+                ...state,
+                genre: 'Genre is required',
+            }));
+        } else {
+            if (errors.genre) {
+                setErrors(state => ({ ...state, genre: '' }));
+            }
+        }
+    };
+
+    const priceValidator = () => {
+        if (Number(values.price) <= 0) {
+            setErrors(state => ({
+                ...state,
+                price: 'Price Should be a positive number',
+            }));
+        } else {
+            if (errors.price) {
+                setErrors(state => ({ ...state, price: '' }));
+            }
+        }
+    };
+
+    const descriptionValidator = () => {
+        if (values.description.length < 5) {
+            setErrors(state => ({
+                ...state,
+                description: 'Description Should be Atleast 5 symbols',
+            }));
+        } else {
+            if (errors.description) {
+                setErrors(state => ({ ...state, description: '' }));
+            }
+        }
+    };
+
+    const imageUrlValidator = () => {
+        if (values.imageUrl.length < 1) {
+            setErrors(state => ({
+                ...state,
+                imageUrl: 'Image is required',
+            }));
+        } else {
+            if (errors.imageUrl) {
+                setErrors(state => ({ ...state, imageUrl: '' }));
+            }
+        }
     };
 
     const { values, onChange, onSubmit } = useForm(createSubmitHandler, initialState);
@@ -34,22 +101,38 @@ export default function CreateGame() {
             <div className={style.createTitle}>Create New Game Offer</div>
 
                 <label htmlFor="title">Name of the Game:</label>
-                <input type="text" id="title" name="title" placeholder="League of Legends, Lethal Company..."  onChange={onChange} value={values.title} />
+                <input type="text" id="title" name="title" placeholder="League of Legends, Lethal Company..."  onChange={onChange} onBlur={titlelValidator} value={values.title} />
+                {errors.title && (
+                                        <p className={style.errorMessage}>{errors.title}</p>
+                                    )}
 
                 <label htmlFor="genre">Genre:</label>
-                <input type="text" name="genre" id="genre" placeholder="Horror, Action..." onChange={onChange} value={values.genre}/>
+                <input type="text" name="genre" id="genre" placeholder="Horror, Action..." onChange={onChange} onBlur={genrelValidator} value={values.genre}/>
+                {errors.genre && (
+                                        <p className={style.errorMessage}>{errors.genre}</p>
+                                    )}
 
                 <label htmlFor="imageUrl">Image:</label>
-                <input type="text" name="imageUrl" id="imageUrl" placeholder="http://..." onChange={onChange} value={values.imageUrl}/>
+                <input type="text" name="imageUrl" id="imageUrl" placeholder="http://..." onChange={onChange} onBlur={imageUrlValidator} value={values.imageUrl}/>
+                {errors.imageUrl && (
+                                        <p className={style.errorMessage}>{errors.imageUrl}</p>
+                                    )}
 
                 <label htmlFor="description">Description:</label>
-                <textarea name="description" id="description" rows="3" cols="50" placeholder="It is most enjoyable with friends..." onChange={onChange} value={values.description}></textarea>
+                <textarea name="description" id="description" rows="3" cols="50" placeholder="It is most enjoyable with friends..." onChange={onChange} onBlur={descriptionValidator} value={values.description}></textarea>
+                {errors.description && (
+                                        <p className={style.errorMessage}>{errors.description}</p>
+                                    )}
 
                 <label htmlFor="price">Price:</label>
-                <input type="number" name="price" id="price" onChange={onChange} value={values.price}/>
+                <input type="number" name="price" id="price" onChange={onChange} onBlur={priceValidator} value={values.price}/>
+                {errors.price && (
+                                        <p className={style.errorMessage}>{errors.price}</p>
+                                    )}
 
                 <div className={style.createSubmit}>
-                    <input className={style.btnSubmit} type="submit" value="Create" />
+                    <input className={style.btnSubmit} disabled={(Object.values(errors).some(x => x)
+                                            || (Object.values(values).some(x => x == '')))} type="submit" value="Create" />
                 </div>
         
             </div>
